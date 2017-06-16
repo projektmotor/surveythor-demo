0937ea0c18c8e188bbe40a169dc133785fb5f7da
Rombo Kraft <kraft@projektmotor.de>
2017-06-16 15:35:47 +0200

    rm commented dumps

--- src/AppBundle/Controller/EvaluationController.php
+++ src/AppBundle/Controller/EvaluationController.php
@@ -11,15 +11,6 @@ class EvaluationController
 {
     public function evaluateResultAction(Result $result)
     {
-        /*
-        foreach ($result->getResultAnswers() as $v) {
-            dump($v);
-            foreach ($v->getChildAnswers() as $vv) {
-                dump($vv);
-            }
-        }
-        die();
-         */
         return array( 'result' => $result);
     }
 }
