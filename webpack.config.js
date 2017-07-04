ffd66556542c30e501a30dca6701a272792630e5
robertfausk <robertfreigang@gmx.de>
2017-07-04 13:11:41 +0200

    add yarn, webpack encore and remove koala framework extra
    Package operations: 0 installs, 0 updates, 2 removals
      - Removing koala-framework/composer-extra-assets (v2.0.8)
      - Removing mouf/nodejs-installer (v1.0.12)

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,49 @@
-var webpack = require('webpack');
-var node_dir = __dirname + '/node_modules';
-var assets_dir = __dirname + '/app/Resources/PMSurveythorBundle/assets';
+let assets_dir = './app/Resources/PMSurveythorBundle/assets';
 
-module.exports = {
-    entry   : {
-        survey: assets_dir + '/js/pages/survey.js',
-        result: assets_dir + '/js/pages/result.js'
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jquery: "jQuery",
-            "window.jQuery": "jquery"
-        })
-    ],
-    resolve: {
-        alias: {
-            'jquery': node_dir + '/jquery/'
-        }
-    },
-    output: {
-        filename: "[name].bundle.js",
-        path : __dirname + 'web/js/',
-        publicPath : '/js/',
-    },
-    devServer: {
-        port: 8080
-    }
-};
+let Encore = require('@symfony/webpack-encore');
+
+Encore
+// directory where all compiled assets will be stored
+    .setOutputPath('web/build/')
+
+    // what's the public path to this directory (relative to your project's document root dir)
+    .setPublicPath(Encore.isProduction() ? '/build' : 'http://surveythor-demo:8080/build/')
+
+    // .setOutputPath()
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // will output as web/build/survey.js
+    .addEntry('survey', assets_dir + '/js/pages/survey.js')
+    .addEntry('result', assets_dir + '/js/pages/result.js')
+
+    // will output as web/build/global.css
+    .addStyleEntry('global', assets_dir + '/css/global.scss')
+
+    // allow sass/scss files to be processed
+    .enableSassLoader({
+        resolve_url_loader: false
+    })
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+
+    .enableSourceMaps(!Encore.isProduction())
+
+    .setManifestKeyPrefix('build/')
+
+    // create hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+;
+
+// export the final configuration
+config = Encore.getWebpackConfig();
+
+if (config.devServer) {
+    config.devServer.host = '0.0.0.0';
+    config.devServer.port = '8080';
+    config.devServer.public = "surveythor-demo:8080";
+}
+
+module.exports = config;
