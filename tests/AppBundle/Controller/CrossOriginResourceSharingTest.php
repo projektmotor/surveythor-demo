abeac35c16e81401af5dd531a962bf37516506b8
robertfausk <robertfreigang@gmx.de>
2017-10-11 14:20:39 +0200

    fix CORS integration test
    by removing param test.surveythor_host cause it's not needed anyway
    and by creating result for survey before trying to load it

--- tests/AppBundle/Controller/CrossOriginResourceSharingTest.php
+++ tests/AppBundle/Controller/CrossOriginResourceSharingTest.php
@@ -17,11 +17,25 @@ class CrossOriginResourceSharingTest extends WebTestCase
             ]
         );
 
-        $uri = $client->getContainer()->getParameter('test.surveythor_host').'/result/next/1/8/1';
+        // create first result for survey
+        $uri = '/result/first/1';
+        $client->request('POST', $uri);
+
+        $this->assertSame(
+            200,
+            $client->getResponse()->getStatusCode(),
+            'wrong status code for uri '.$client->getRequest()->getUri()
+        );
+
+        $uri = '/result/next/1/8/1';
 
         $client->request('POST', $uri);
 
-        $this->assertSame(200, $client->getResponse()->getStatusCode(), 'wrong status code for uri '.$uri);
+        $this->assertSame(
+            200,
+            $client->getResponse()->getStatusCode(),
+            'wrong status code for uri '.$client->getRequest()->getUri()
+        );
 
         $this->assertTrue(
             $client->getResponse()->headers->has('Access-Control-Allow-Origin'),
