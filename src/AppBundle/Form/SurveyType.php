4dbaac17d49b8408a89c44b9cfc44b81c16d39b4
robertfausk <robertfreigang@gmx.de>
2017-10-31 10:20:56 +0100

    add missing use statement in SurveyType

--- src/AppBundle/Form/SurveyType.php
+++ src/AppBundle/Form/SurveyType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use AppBundle\Entity\Survey;
+use AppBundle\Form\SurveyItems\QuestionCollectionType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
