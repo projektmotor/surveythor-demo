8a02bfd1282d37f645607f1c41543a41135e49e9
pueppiblue <Nils.Freigang@gmail.com>
2017-11-09 02:27:54 +0100

    add routes for embedded editing, executing and evaluating of a survey

--- src/AppBundle/Controller/EmbedController.php
+++ src/AppBundle/Controller/EmbedController.php
@@ -4,16 +4,37 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Entity\Survey;
+use AppBundle\Repository\SurveyRepository;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\Form\FormFactory;
 
 class EmbedController
 {
+    /** @var SurveyController */
+    private $surveyController;
+    private $resultController;
 
-    public function showAction(Survey $survey): array
+    public function __construct(SurveyController $surveyController, ResultController $resultController)
     {
-        {
-            return [
-                'survey' => $survey,
-            ];
-        }
+        $this->surveyController = $surveyController;
+        $this->resultController = $resultController;
+    }
+
+    public function executeSurveyAction(Survey $survey): array
+    {
+        return $this->resultController->newAction($survey);
+    }
+
+
+    public function editSurveyAction(Survey $survey): array
+    {
+
+        return $this->surveyController->editAction($survey);
+    }
+
+    public function evaluateSurveyAction(Survey $survey)
+    {
+        return $this->surveyController->evaluationsAction($survey);
+
     }
 }
